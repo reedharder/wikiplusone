4caa5fe72baf89a2c407cb46ab018a05425a9705
Reed Harder <reedharder@gmail.com>
2014-06-13 12:00:27 -0400

    wiki parser with refined archive url functionality

--- parser_multiwikipage_xml.py
+++ parser_multiwikipage_xml.py
@@ -40,30 +40,45 @@ def dump_parser(file="DdumpAlgebra.xml", txt_out="wikilinks1.txt"):
             # a very rough parsing of wiki links 
             re_links=re.findall("\[\[.*?\]\]",wikitext) #get list of all non overlapping [[..]] patterns
             wikilinks=[] #list of wiki links
+            categories=[]
             for link in re_links:
                 nopiclink=link.split("[[")[-1][:-2] # remove initial '[[', and any picture/file text, and remove final ']]'
                 no_alias=nopiclink.split("|")[0] # take only the actual link, not what is displayed
-                if (no_alias[:5] != 'File:' and no_alias[:6] != 'Image:'):
+                if (no_alias[:9] == 'Category:'):
+                     categories.append(no_alias)
+                else:
                     wikilinks.append(no_alias) #still leaves categories, pics/files with no wikilinks in caption, and subsections. Pics at least can be parsed out
             
-            re_footnote_refs=re.findall("<ref>.*?</ref>",wikitext) 
+            re_footnote_refs=re.findall("<ref.*?>.*?</ref>",wikitext) 
             #JUST IGNORE WEB ARCHIVES PERHAPS? NO, NON ARCHIVES WILL BE BROKEN. TRY TAKING JUST LAST LINK
             foot_links=[] #list of footnote links, or archives if those exist
             for ref in re_footnote_refs:
-                try:
-                     #find list of things after http:// or https:// and up to but not including either a space or "|" or "]", and only takes final such item, assuming that this will be the archive copy if it exists, and the normal link if not. Hopefully these are good assumptions 
-                    link=re.findall("http://.*?(?=[|\s\]])|https://.*?(?=[|\s\]])",ref)[-1]
-                     #HOLD UP <ref name=name>content</ref> ADD THIS KIND OF CITATION TO REG EXPRESSIONS
-                    foot_links.append(link)
-                except IndexError:
+                #find list of things after http:// or https:// and up to but not including either a space or "|" or "]"
+                link=re.findall("http://.*?(?=[|\s\]])|https://.*?(?=[|\s\]])",ref)
+                # if no links are found in ref, go to next ref
+                if len(link) == 0:
                     pass
-            
+                #if one link is found, take as footnote and append to list
+                elif len(link) == 1:
+                    foot_links.append(link[0])
+                # if multiple links are found, check a little deeper
+                else: 
+                    #search for links labled archive
+                    arch_link=re.findall("archiveurl\s*=\s*http://.*?(?=[|\s\]])|archiveurl\s*=\s*https://.*?(?=[|\s\]])",ref)
+                    #search for links labled with standard "url="
+                    reglink=re.findall("\|\s*url\s*=\s*http://.*?(?=[|\s\]])|\|\s*url\s*=\s*https://.*?(?=[|\s\]])",ref)
+                    #add archive links to footnote list
+                    if len(arch_link) > 0:
+                        foot_links.append(re.sub("archiveurl\s*=\s*",'',arch_link[0],count=0))
+                    #if there are standard links as well, add them if they have been marked as not broken (i.e. ref has label "deadurl=no")    
+                    if len(reglink) > 0 and re.search("deadurl\s*=\s*no",ref): 
+                        foot_links.append(re.sub("\|\s*url\s*=\s*",'',reglink[0],count=0))
             #FOR NOW, write links unorganized to text file (LATER, SQL DATABASE PROBABLY)
                  
             for link in foot_links:
                 outfile.write("%s\n" % link)
             #stick data into pandas DataFrame (MAKE SURE IT WON'T GET TO BIG)      
-            wiki_df.loc[i]=pd.Series({'title': title_text, 'wikilinks': wikilinks, 'footnotes':foot_links, 'num_feet': len(foot_links)})
+            wiki_df.loc[i]=pd.Series({'title': title_text, 'wikilinks': wikilinks, 'categories': categories, 'footnotes':foot_links, 'num_feet': len(foot_links)})
 
 #NOW GET OTHERTYPES OF LINKS: EXTERNAL, ADDITIONAL FOOT NOTES, ETC. OTHER DATA? REDIRECTS?
 #GET JUST DOMAINS OF LINKS ALSO? PROBABLY WAIT UNTIL FINAL ANALYSIS
